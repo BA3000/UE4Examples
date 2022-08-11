372322e7ebc7c3707bea4f846b0e2766b1df9dfa
BA3000 <2864281096@qq.com>
2022-08-11 15:26:34 +0800

    add: retarget target

---
+++ UFUNCTIONExamples/Source/UfunctionExamples/ExecTargetPawn.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExecTargetPawn.h"
+
+
+// Sets default values
+AExecTargetPawn::AExecTargetPawn()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AExecTargetPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AExecTargetPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AExecTargetPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AExecTargetPawn::RetargetExecFunc(const float Num)
+{
+	UE_LOG(LogTemp, Display, TEXT("Retarget Exec Func Called! Got Num: %f"), Num);
+}
+
---
+++ UFUNCTIONExamples/Source/UfunctionExamples/ExecTargetPawn.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "ExecTargetPawn.generated.h"
+
+UCLASS()
+class UFUNCTIONEXAMPLES_API AExecTargetPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AExecTargetPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(Exec)
+	void RetargetExecFunc(float Num);
+};
