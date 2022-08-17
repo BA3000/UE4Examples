4e384e05b8881093c138f7cb3d736af32dc85738
BA3000 <2864281096@qq.com>
2022-08-17 09:41:45 +0800

    add: blueprint function library

---
+++ UFUNCTIONExamples/Source/UfunctionExamples/ExampleBPFuncLibrary.cpp
@@ -0,0 +1,12 @@
+﻿#include "ExampleBPFuncLibrary.h"
+
+void UExampleBPFuncLibrary::BPCallableExampleFunc()
+{
+	UE_LOG(LogTemp, Display, TEXT("=== Info: Print from BPCallableExampleFunc ==="));
+}
+
+int UExampleBPFuncLibrary::BPPureExampleFunc(const int L, const int R)
+{
+	UE_LOG(LogTemp, Display, TEXT("=== Info: BPPureExampleFunc Called! ==="));
+	return L + R;
+}
---
+++ UFUNCTIONExamples/Source/UfunctionExamples/ExampleBPFuncLibrary.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "ExampleBPFuncLibrary.generated.h"
+
+UCLASS()
+class UFUNCTIONEXAMPLES_API UExampleBPFuncLibrary final : public  UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static void BPCallableExampleFunc();
+
+	UFUNCTION(BlueprintPure)
+	static int BPPureExampleFunc(int L, int R);
+};
